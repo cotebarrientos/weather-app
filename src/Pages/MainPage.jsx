3cfaa5eea92a53c64570b0f705d07747c6259281
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-08-07 18:35:05 +0100

    Add useHistory hook inside MainPage file

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const MainPage = () => {
+    const history= useHistory()
+
+    const onClickHandler = () => {
+        // history.push permite alterar la URL por programación
+        history.push('/city')
+    }
+
     return (
         <div>
             Main
+            <button onClick={onClickHandler}>Go to City Page</button>
         </div>
     )
 }
