948c3e471a2776b8249aa7af92890325dc4f12ce
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-08-07 17:48:54 +0100

    Add React Router to App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, 
+    Switch,
+    Route } from 'react-router-dom'
+
+const App = () => {
+    return (
+        <div>
+            <h1>My App</h1>
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        Welcome
+                    </Route>
+                    <Route path='/main'>
+                        Main
+                    </Route>
+                    <Route path='/city'>
+                        City
+                    </Route>
+                    <Route>
+                        Not Found
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+export default App
