4b683cbd3a3fd8bf7ae11f79bba37def5fa3f779
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-08-13 22:33:55 +0100

    Update NotFoundPage, adding new content

--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -1,14 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
+import { Link as RouterLink } from 'react-router-dom'
+import { IconContext } from 'react-icons'
+import { WiRain } from 'react-icons/wi'
 
 const NotFound = () => {
     return (
-        <div>
-            Not Found
-            <div>
-                <Link to='/main'>Back to Main</Link>
+        <Grid container
+            direction='column'
+            justifyContent='center'
+            className='full'>
+            <div className="highlight">
+                <Grid item container xs={12}
+                    justifyContent='center'
+                    alignItems='center'>
+                    <Grid item
+                        container
+                        direction='column'
+                        justifyContent='center'
+                        alignItems='center'>
+                        <Typography variant='h2' color='inherit'>
+                            404
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <IconContext.Provider value={{ size:'6em' }}>
+                            <WiRain />
+                        </IconContext.Provider>
+                    </Grid>
+                    <Grid item
+                        container
+                        direction='column'
+                        justifyContent='center'
+                        alignItems='center'>
+                        <Typography variant='h4' color='inherit'>
+                            Page not found!
+                        </Typography>
+                        <Link color='inherit'
+                            aria-label='menu'
+                            component={RouterLink}
+                            to='/main'>
+                            Go to Main
+                        </Link>
+                    </Grid>
+                </Grid>
             </div>
-        </div>
+        </Grid>
     )
 }
 
