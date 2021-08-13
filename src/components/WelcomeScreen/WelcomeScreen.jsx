f376b58d42d8c7944e08061b91d048b6f408a0a2
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-08-13 20:49:40 +0100

    Add Clouds effect to WelcomeScreen component

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import Clouds from 'vanta/dist/vanta.clouds.min'
+import * as THREE from 'three'
 
 const WelcomeScreen = ({children}) => {
+    const myRefDiv = useRef(null)
+    const [vanta, setVanta] = useState(0)
+    console.log("myRefDiv.current", myRefDiv.current)
+
+    useEffect(() => {
+        console.log("myRefDiv.current(in useEffect)", myRefDiv.current)
+
+        if (!vanta) {
+            setVanta(Clouds ({
+                THREE,
+                el: myRefDiv.current    
+            }))
+            console.log("Different value to 0")
+        }
+        return () => {
+            if (vanta) {
+                vanta.destroy()
+                console.log('space freed')
+            }
+        }
+    },[vanta])
+
     return (
-        <div>
+        <div ref={myRefDiv}>
             Welcome Screen
         </div>
     )
