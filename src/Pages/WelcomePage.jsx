462f21096656e47653e5920f4bb219f2546e8d2e
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-08-13 22:24:45 +0100

    Add CSS improvements

--- src/Pages/WelcomePage.jsx
+++ src/Pages/WelcomePage.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import WelcomeScreen from './../components/WelcomeScreen'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
+import { Link as RouterLink } from 'react-router-dom'
+import { IconContext } from 'react-icons'
+import { WiDaySunny } from 'react-icons/wi'
 
 const WelcomePage = () => {
     return (
         <WelcomeScreen>
-
+            <Grid container
+                direction='column'
+                justifyContent='center'
+                className='full'>
+                <div className="highlight">
+                    <Grid item container xs={12}
+                        justifyContent='center'
+                        alignItems='center'>
+                        <Grid item
+                            container
+                            direction='column'
+                            justifyContent='center'
+                            alignItems='center'>
+                            <Typography variant='h2' color='inherit'>
+                                Welcome to
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <IconContext.Provider value={{ size:'6em' }}>
+                                <WiDaySunny />
+                            </IconContext.Provider>
+                        </Grid>
+                        <Grid item
+                            container
+                            direction='column'
+                            justifyContent='center'
+                            alignItems='center'>
+                            <Typography variant='h4' color='inherit'>
+                                Weather App
+                            </Typography>
+                            <Link color='inherit'
+                                aria-label='menu'
+                                component={RouterLink}
+                                to='/main'>
+                                Visit Us
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </div>
+            </Grid>
         </WelcomeScreen>
     )
 }
