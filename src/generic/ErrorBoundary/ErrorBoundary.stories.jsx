e4f2b4871e7d10d33d05d24b63939c8733cef3a6
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-08-17 22:50:44 +0100

    Add Error Boundary Component

---
+++ src/generic/ErrorBoundary/ErrorBoundary.stories.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ErrorBoundary from './ErrorBoundary'
+
+export default {
+    title: 'ErrorBoundary',
+    component: ErrorBoundary
+}
+
+const ComponentWithoutError = () => <h1>Without Error</h1>
+
+
+const prop = undefined
+
+const ComponentWithError = () => <h1>{prop.hello}</h1>
+
+
+export const ErrorBoundaryWithError = () => (
+    <ErrorBoundary>
+        <ComponentWithError />
+    </ErrorBoundary>
+)
+
+export const ErrorBoundaryWithoutError = () => (
+    <ErrorBoundary>
+        <ComponentWithoutError />
+    </ErrorBoundary>
+)
