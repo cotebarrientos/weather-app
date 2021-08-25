284c60ec4acfab7b39571de320a0edec6c2ac9d7
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-08-25 17:31:55 +0100

    Add code refactor for useCityPage hook

--- src/hooks/useCityPage.jsx
+++ src/hooks/useCityPage.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import moment from 'moment'
 import { useParams } from 'react-router-dom'
 import { getForecastUrl } from './../utils/urls'
-import { toCelsius } from './../utils/utils'
+import getChartData from './../utils/transform/getChartData'
+import getForecastItemList from './../utils/transform/getForecastItemList'
 
 const useCityPage = () => {
     const [chartData, setChartData] = useState(null)
@@ -17,46 +17,13 @@ const useCityPage = () => {
 
             try {
                 const { data } = await axios.get(url)
+
+                const dataAux = getChartData(data)
                 
-                console.log('data', data)
-
-                const daysAhead = [0, 1, 2, 3, 4, 5]
-                const days = daysAhead.map(d => moment().add(d, 'd'))
-                const dataAux = days.map(day => {
-
-                    const tempObjArray = data.list.filter(item => {
-                        const dayOfYear = moment.unix(item.dt).dayOfYear()
-                        return dayOfYear === day.dayOfYear()
-                    })
-
-                    console.log('day.dayOfYear()', day.dayOfYear())
-                    console.log('tempObjArray', tempObjArray)
-
-                    const temps = tempObjArray.map(item => item.main.temp)
-
-                    // dayHour, min, max.
-                    return ({
-                        dayHour: day.format('ddd'),
-                        min: toCelsius(Math.min(...temps)),
-                        max: toCelsius(Math.max(...temps)),
-                        hasTemps: (temps.length > 0 ? true : false)
-                    })
-                }).filter(item => item.hasTemps)
-
                 setChartData(dataAux)
 
-                const interval = [4, 8, 12, 16, 20, 24]
-                const forecastItemListAux = data.list
-                    .filter((item, index) => interval.includes(index))
-                    .map(item => {
-                        return ({
-                            hour: moment.unix(item.dt).hour(),
-                            weekDay: moment.unix(item.dt).format('dddd'),
-                            state: item.weather[0].main.toLowerCase(),
-                            temperature: toCelsius(item.main.temp),
-                        })
-                })
-                
+                const forecastItemListAux = getForecastItemList(data)
+
                 setForecastItemList(forecastItemListAux)
 
             } catch (error) {
