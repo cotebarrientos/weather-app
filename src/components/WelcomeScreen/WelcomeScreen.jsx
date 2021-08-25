25c3633963381c6c4db227d47d47c04c45c2486b
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2021-08-25 15:17:17 +0100

    Add useVanta custom hook

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,30 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import Clouds from 'vanta/dist/vanta.clouds.min'
-import * as THREE from 'three'
+import useVanta from './../../hooks/useVanta'
 
 const WelcomeScreen = ({children}) => {
-    const myRefDiv = useRef(null)
-    const [vanta, setVanta] = useState(0)
-    console.log("myRefDiv.current", myRefDiv.current)
 
-    useEffect(() => {
-        console.log("myRefDiv.current(in useEffect)", myRefDiv.current)
-
-        if (!vanta) {
-            setVanta(Clouds ({
-                THREE,
-                el: myRefDiv.current    
-            }))
-            console.log("Different value to 0")
-        }
-        return () => {
-            if (vanta) {
-                vanta.destroy()
-                console.log('space freed')
-            }
-        }
-    },[vanta])
+    const myRefDiv = useVanta()
 
     return (
         <div className='full' ref={myRefDiv}>
